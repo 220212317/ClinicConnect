c322754f47cfe74e5555eeeb0b744f22ceafc69f
Athi Sintiya <220212317@mycput.ac.za>
2026-07-22 22:34:36 +0200

    Fixed config.resolver

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add support for additional file extensions
-config.resolver.assetExts.push('cjs');
+
+config.resolver.sourceExts.push('cjs');
 
 module.exports = config;
