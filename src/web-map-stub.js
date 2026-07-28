4d7e0e5c7955ae4a80f6788c1db394fe979952a9
shibambocollins <ntsobokwanec@gmail.com>
2026-07-28 20:56:28 +0200

    chore: add web-map-stub.js to prevent react-native-maps breaking web bundling

---
+++ src/web-map-stub.js
@@ -0,0 +1,8 @@
+// Temporary stub so react-native-maps doesn't break web bundling
+module.exports = {
+  default: () => null,
+  Marker: () => null,
+  Polygon: () => null,
+  Callout: () => null,
+  PROVIDER_GOOGLE: 'google',
+};
